306a5647ce30dd5f6b2cc28d34b5a74a67a87d0b
JuniorThanh <2351050164thanh@ou.edu.vn>
2025-12-04 17:00:01 +0700

    AddScriptsRun

---
+++ Scripts.sh
@@ -0,0 +1,10 @@
+if [ -f "venv/Scripts/activate" ]; then
+    echo "Đã tồn tại môi trường ảo"
+else
+    echo "Tạo môi trường ảo"
+    python -m venv venv
+fi
+
+source venv/Scripts/activate
+
+pip install -r requirements.txt
